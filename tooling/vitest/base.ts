9317a7d67f28b0fae69ae929f57f6bc5e2235595
gudfhr95 <ghldnjsdlf@gmail.com>
2025-07-29 02:43:37 +0900

    chore: add vitest

---
+++ tooling/vitest/base.ts
@@ -0,0 +1,8 @@
+import type { ViteUserConfig } from "vitest/config";
+
+export default {
+  test: {
+    globals: true,
+    include: ["test/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+  },
+} satisfies ViteUserConfig;
